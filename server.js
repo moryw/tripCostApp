04246ad9d179aa4ca8ce3d84108e1da35e0457fd
Mory W <matt@skitskat.xyz>
2020-07-04 09:19:03 -0500

    Implement POST expense and GET expenses by Trip _ID

--- server.js
+++ server.js
@@ -32,10 +32,33 @@ app.get('/trips', (req, res) => {
   });
 });
 app.post('/expense', (req, res) => {
-  /* */
+  expenses.insertOne(
+    {
+      trip: req.body.trip,
+      date: req.body.date,
+      amount: req.body.amount,
+      category: req.body.category,
+      description: req.body.description
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err);
+        res.status(500).json({err:err});
+        return;
+      }
+      res.status(200).json({ok:true});
+    }
+  )
 });
 app.get('/expenses', (req, res) => {
-  /* */
+  expenses.find({trip: req.body.trip}).toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      re.status(500).json({err: err});
+      return;
+    }
+    res.status(200).json({trips: items});
+  })
 });
 
 let db, trips, expenses
