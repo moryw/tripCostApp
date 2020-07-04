f63186e0f75f572e836e2ca75e2cc2ea5e0c00a0
Mory W <matt@skitskat.xyz>
2020-07-03 22:33:02 -0500

    Add mongo url and connection

--- server.js
+++ server.js
@@ -3,10 +3,26 @@ const mongo = require('mongodb').MongoClient;
 
 const app = express();
 
+// Start Server
+app.listen(3000, () => console.log('Server Running') );
 
 app.post('/trip', (req, res) => {/* */});
 app.get('/trips', (req, res) => {/* */});
 app.post('/expense', (req, res) => {/* */});
 app.get('/expenses', (req, res) => {/* */});
 
-app.listen(3000, () => console.log('Server Running') );
+
+const url = 'mongodb://localhost:27017'
+
+let db
+
+mongo.connect(
+  url,
+  (err, client) => {
+    if (err) {
+      console.error(err);
+      return ;
+    }
+    db = client.db('tripcost');
+  }
+)
