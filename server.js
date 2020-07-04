74bbc96194783ce03daef6bce870b523f46bc35e
Mory W <matt@skitskat.xyz>
2020-07-04 08:56:19 -0500

    Implement POST trip and GET trips

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const mongo = require('mongodb').MongoClient;
+const url = 'mongodb://localhost:27017';
 
 const app = express();
 
 // Start Server
 app.listen(3000, () => console.log('Server Running') );
 
-app.post('/trip', (req, res) => {/* */});
-app.get('/trips', (req, res) => {/* */});
-app.post('/expense', (req, res) => {/* */});
-app.get('/expenses', (req, res) => {/* */});
+app.use(express.json());
 
-
-const url = 'mongodb://localhost:27017'
+app.post('/trip', (req, res) => {
+  const name = req.body.name;
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({err:err});
+      return;
+    }
+    console.log(result);
+    res.status(200).json({ok:true});
+  });
+});
+app.get('/trips', (req, res) => {
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err);
+      res.status(500).json({err: err});
+      return;
+    }
+    res.status(200).json({trips:items});
+  });
+});
+app.post('/expense', (req, res) => {
+  /* */
+});
+app.get('/expenses', (req, res) => {
+  /* */
+});
 
 let db, trips, expenses
 
